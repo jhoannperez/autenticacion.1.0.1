041e9df1a1f514d5949dce44d88bbf9571cc7077
jhoannperez <jhoanncamilop@gmail.com>
2024-05-22 15:07:59 -0500

    modified:   src/hooks.js modified:   src/lib/stores.js

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,11 +1,7 @@
 // @ts-nocheck
 import { writable } from 'svelte/store';
 
-// Intentar cargar la sesión desde el almacenamiento local
-const storedSession = localStorage.getItem('supabaseSession');
-const initialUser = storedSession ? JSON.parse(storedSession).user : null;
-
-export const user = writable(initialUser);
+export const user = writable(null);
 
 export function setUser(newUser) {
     user.set(newUser);
